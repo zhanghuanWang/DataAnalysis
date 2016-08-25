702916fb9c4a9728d91a4adcf3cfe1fbb929ede6
wzh <wang.zhanghuan@cesgroup.com.cn>
2016-08-25 15:48:31 +0800

    init

---
+++ src/main/java/cn/laolema/controller/UserController.java
@@ -0,0 +1,11 @@
+package cn.laolema.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by wzh on 16-8-25.
+ */
+@Controller("/login")
+public class UserController {
+
+}
---
+++ src/main/java/cn/laolema/entity/UserEntity.java
@@ -0,0 +1,9 @@
+package cn.laolema.entity;
+
+/**
+ * Created by wzh on 16-8-25.
+ */
+public class UserEntity {
+
+    private String name;
+}
